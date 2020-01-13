ddae13537ca676fb435946dac898a4c516d05a55
Andrey Gaichikov <gaichikov@gmail.com>
2020-01-13 21:40:47 +0000

    Installation script, initial.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # opencv_compile
 OpenCV compiler
+
+
+usage: opencv_compile.py [-h] [--cv_ver CV_VER] [--py_ver PY_VER]
+
+Script for compiling opencv from source.
+
+optional arguments:
+  -h, --help       show this help message and exit
+  --cv_ver CV_VER  Opencv version: 3.4.7, 4.1
+  --py_ver PY_VER  Python version: 2.7, 3
+
+
+To compile for python3 - create virtual environment and activate: 
+
+```virtualenv -p python3 venv_3```
+```source venv_3/bin/activate```
+
+Then launch the script:
+
+```sudo python opencv_compile.py --cv_ver 3.4.7```
+
+
+To compile for python2.7 
+```virtualenv -p python2 venv_2```
+```source venv_2/bin/activate```
+```sudo python opencv_compile.py --cv_ver 3.4.7```
---
+++ opencv_compile.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import argparse
+
+cv_versions = ['3.4.7', '4.1']
+
+parser = argparse.ArgumentParser(description='Script for compiling opencv from source.')
+parser.add_argument('--cv_ver', dest='cv_ver', help='Opencv version: {} '.format(', '.join(cv_versions)))
+parser.add_argument('--py_ver', dest='py_ver', help='Python version: 2.7, 3')
+
+args = parser.parse_args()
+if not args.cv_ver:
+    print('Please specify opencv version. \n')
+    print(parser.print_help())
+    sys.exit()
+
+print('Updating repositories')
+os.system('apt-get update && apt-get -y upgrade')
+
+print('Installing required packages..')
+os.system('apt-get install -y build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-serial-dev libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5 python-dev python3-dev ')
+
+print('Installing required pip packages')
+os.system('pip install numpy')
+
+print('Downloading opencv packages')
+os.system('wget -O opencv.zip https://github.com/opencv/opencv/archive/{}.zip && unzip -o opencv.zip && wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/{}.zip && unzip -o opencv_contrib.zip'.format(args.cv_ver, args.cv_ver))
+
+os.chdir('opencv-{}'.format(args.cv_ver))
+# try:
+#     os.rmdir('build')
+# except:
+#     pass
+try:
+    os.mkdir('build')
+except:
+    pass
+os.chdir('build')
+
+print(os.getcwd())
+
+print('Changing swap size..')
+os.system('sed -i "s/CONF_SWAPSIZE=100/CONF_SWAPSIZE=2048/g" /etc/dphys-swapfile') 
+os.system('/etc/init.d/dphys-swapfile restart')
+
+print('Configuring opencv..')
+
+os.system('cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-{}/modules -D ENABLE_NEON=ON -D ENABLE_VFPV3=ON -D BUILD_TESTS=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D OPENCV_ENABLE_NONFREE=ON -D CMAKE_SHARED_LINKER_FLAGS="-latomic" -D BUILD_EXAMPLES=OFF -D WITH_GSTREAMER=OFF ..'.format(args.cv_ver))
+print('Compiling..')
+os.system('make -j2 && make install && ldconfig')
+
+print('Return back swap size..')
+os.system('sed -i "s/CONF_SWAPSIZE=2048/CONF_SWAPSIZE=100/g" /etc/dphys-swapfile') 
+os.system('/etc/init.d/dphys-swapfile restart')
+
+print('Done!')
